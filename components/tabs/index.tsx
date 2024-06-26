9403e1ee8bfb93b018da533053e30bfa554a8888
qbentil <bentilshadrack72@gmail.com>
2024-06-26 09:35:06 +0000

    Make tabs responsive

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -16,13 +16,13 @@ const Tabs = ({ tabs, setTab, tab }: TabsProps) => {
             {tabs.map((item) => (
                 <div
                     key={item.name}
-                    className={`flex items-center justify-center text-sm font-semibold text-gray-500 mr-4 p-2 gap-x-2 m-0 cursor-pointer hover:text-primary-600 border-b-2 border-transparent ${tab === item.name
+                    className={`flex items-center justify-center text-xs md:text-sm font-semibold text-gray-500 mr-4 p-2 gap-x-2 m-0 cursor-pointer hover:text-primary-600 border-b-2 border-transparent ${tab === item.name
                         ? "border-secondary-600 text-primary-600 border-b-2 border-b-primary-600  "
                         : ""
                         }`}
                     onClick={() => setTab(item.name as string)}
                 >
-                    {item.Icon && <item.Icon variant="TwoTone" />}
+                    {item.Icon && <item.Icon variant="TwoTone" className="hidden md:block" />}
                     {item.label}
                 </div>
             ))}
